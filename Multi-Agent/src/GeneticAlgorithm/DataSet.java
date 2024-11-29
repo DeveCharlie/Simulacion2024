2fc7f6f66745882a5b5ee8d6f78232d1be8de32e
carlos alberto montes medina <carlosalbertomontesmedina2@gmail.com>
2024-11-28 23:00:36 -0600

    Starting with the Genetic Algorithim

---
+++ Multi-Agent/src/GeneticAlgorithm/DataSet.java
@@ -0,0 +1,16 @@
+package GeneticAlgorithm;
+
+public class DataSet {
+
+    protected double[] x, y;
+    protected int numCoeficientes;
+
+    public DataSet(double [] x, double[] y, int numCoeficientes){
+        this.x = x;
+        this.y = y;
+        this.numCoeficientes = numCoeficientes;
+    }
+
+    
+
+}
---
+++ Multi-Agent/src/GeneticAlgorithm/GeneticParameters.java
@@ -0,0 +1,11 @@
+package GeneticAlgorithm;
+
+public class GeneticParameters {
+
+    protected int population = 50; // 50%
+    protected double crossOverRate = 0.90; // 90%
+    protected double mutationRate = 0.10; // 10%
+    protected double elitism = 0.05; // 5%
+
+
+}
